2d6b2305dbe149eb320d6f24460c1e13415ad510
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-05-17 21:06:35 -0300

    add: product-item estilos

--- script/productos.js
+++ script/productos.js
@@ -1,37 +1,46 @@
 const obtenerProductos = async () => {
-    const products = []
-    const data = await fetch("../data/data.json");
-    const json = await data.json();
-    
-    json.shop.forEach(e => {
-      products.push(e)
-    });
-  
-    return products
-  }
+  const products = [];
+  const data = await fetch("../data/data.json");
+  const json = await data.json();
+
+  json.shop.forEach((e) => {
+    products.push(e);
+  });
+
+  return products;
+};
 
 //funcion para mostrar los productos
 const mostrarProductos = async (containerProductId) => {
+  //consumo la api
+  const productos = await obtenerProductos();
 
-    //consumo la api
-    const productos = await obtenerProductos()
-  
-    const containerProduct = document.getElementById(containerProductId);
-  
-    //por cada element de la api creo un div
-    productos.forEach(producto => {
-        const element = document.createElement("div");
-  
-        //creo lo que se vera en el div
-        element.innerHTML = `
-            <h2>${producto.name}</h2>
-            <img src="${producto.img}" alt="${producto.name}">
-            <p>${producto.description}</p>
-            <h5>${producto.type}</h5>
+  const containerProduct = document.getElementById(containerProductId);
+
+  //por cada element de la api creo un div
+  productos.forEach((producto) => {
+    const element = document.createElement("div");
+    element.classList.add("product-item");
+
+    //creo lo que se vera en el div
+    element.innerHTML = `
+            <figure class="product-item-img">
+              <img src="${producto.img}" alt="${producto.name}">
+            </figure>
+            <div class="product-item-body">
+              <h3>${producto.name}</h3>
+              <span>$200</span>
+            </div>
         `;
-  
-        containerProduct.appendChild(element);
-    });
-}
+    // element.innerHTML = `
+    //         <img src="${producto.img}" alt="${producto.name}">
+    //         <h3>${producto.name}</h3>
+    //         <p>${producto.description}</p>
+    //         <h5>${producto.type}</h5>
+    //     `;
+
+    containerProduct.appendChild(element);
+  });
+};
 
 mostrarProductos("shop-container");
