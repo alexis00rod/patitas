b78d50046259552420a988cfa18a458319706768
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-05-19 21:15:06 -0300

    add: estilos service-list

--- script/servicios.js
+++ script/servicios.js
@@ -1,39 +1,48 @@
 const obtenerServicios = async () => {
-  const service = []
+  const service = [];
   const data = await fetch("../data/data.json");
   const json = await data.json();
 
-  json.service.forEach(e => {
-    service.push(e)
+  json.service.forEach((e) => {
+    service.push(e);
   });
 
-  return service
-}
+  return service;
+};
 
 //Funcion para mostrar los servicios
 const mostrarServicios = async (containerServiceId) => {
-
   //consumo la api
-  const servicios = await obtenerServicios()
+  const servicios = await obtenerServicios();
 
   //obtengo el container del DOM
   const containerService = document.getElementById(containerServiceId);
 
   //por cada element de la api creo un div
-  servicios.forEach(servicio => {
-    const elemento = document.createElement("div");
+  servicios.forEach((servicio) => {
+    const elemento = document.createElement("li");
+    elemento.classList.add("service-item");
 
     //creo la estructura a mostrar
     elemento.innerHTML = `
-            <h2>${servicio.title}</h2>
-            <img src="${servicio.img}" alt="${servicio.title}">
-            <p>${servicio.description}</p>
-            <h5>${servicio.type}</h5>
+            <figure class="service-item-img">
+              <img src="${servicio.img}" alt="${servicio.title}">
+            </figure>
+            <div class="service-item-body">
+              <h3>${servicio.title}</h3>
+              <p>${servicio.description}</p>
+            </div>
         `;
+    // elemento.innerHTML = `
+    //         <h2>${servicio.title}</h2>
+    //         <img src="${servicio.img}" alt="${servicio.title}">
+    //         <p>${servicio.description}</p>
+    //         <h5>${servicio.type}</h5>
+    //     `;
 
     //creo los elementos en el HTML apenas de cargan en el DOM
     containerService.appendChild(elemento);
   });
-}
+};
 
 mostrarServicios("service-container");
