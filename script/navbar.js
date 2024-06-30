462b1f7f9ffac15fa970fbbebdbe099300700f5a
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-06-30 18:37:19 -0300

    limpie codigo

--- script/navbar.js
+++
@@ -1,8 +0,0 @@
-// Navbar elementos
-const navbarButton = document.getElementById("navbar-button");
-const navbarContainer = document.getElementById("navbar-container");
-
-// Funcion abrir navbar
-const openNavbar = () => {
-  navbarContainer.classList.toggle("open");
-};
