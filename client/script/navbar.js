4c7b05fbc10545127004a9ca6a506397ea366c63
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-07-01 20:28:55 -0300

    limpie codigo

--- client/script/navbar.js
+++ client/script/navbar.js
@@ -1,8 +1,73 @@
-// Navbar elementos
-const navbarButton = document.getElementById("navbar-button");
-const navbarContainer = document.getElementById("navbar-container");
+// Funcion para crear el navbar
+document.addEventListener("DOMContentLoaded", () => {
+  // Elementos del navbar
+  const header = document.createElement("header");
+  const nav = document.createElement("nav");
+  nav.classList.add("navbar");
 
-// Funcion abrir navbar
-const openNavbar = () => {
-  navbarContainer.classList.toggle("open");
+  // Logo
+  const logo = document.createElement("h1");
+  logo.classList.add("logo");
+  const logoLink = document.createElement("a");
+  logoLink.href = "/index.html";
+  const logoImg = document.createElement("img");
+  logoImg.src = "/client/assets/patitas-logo.png";
+  logoImg.alt = "Patitas logo";
+  const logoText = document.createElement("span");
+  logoText.textContent = "Patitas";
+  logoLink.appendChild(logoImg);
+  logoLink.appendChild(logoText);
+  logo.appendChild(logoLink);
+
+  // Contenedor de enlaces
+  const navbarContainer = document.createElement("ul");
+  navbarContainer.id = "navbar-container";
+  navbarContainer.classList.add("navbar-container");
+
+  // Enlaces
+  const items = [
+    { text: "Inicio", href: "/index.html" },
+    { text: "Productos", href: "/client/pages/productos.html" },
+    { text: "Sobre nosotros", href: "/client/pages/sobre-nosotros.html" },
+    { text: "Contactanos", href: "/client/pages/contactanos.html" },
+    { text: "Iniciar sesión", href: "/client/pages/login.html" },
+  ];
+
+  // Funcion para crear items del navbar
+  const createNavbarItem = (content) => {
+    const { text, href } = content;
+    const item = document.createElement("li");
+    item.classList.add("navbar-item");
+    const link = document.createElement("a");
+    link.href = href;
+    link.textContent = text;
+    item.appendChild(link);
+    return item;
+  };
+
+  // Recorrer array items y crear item del navbar por cada elemento
+  items.map((item) => navbarContainer.appendChild(createNavbarItem(item)));
+
+  // Botón para abrir el navbar en resoluciones menores a 1000px
+  const navbarButton = document.createElement("button");
+  navbarButton.type = "button";
+  navbarButton.classList.add("navbar-button");
+  navbarButton.setAttribute("onclick", "openNavbar()");
+  const navbarIcon = document.createElement("i");
+  navbarIcon.classList.add("fa-solid", "fa-bars");
+  navbarButton.appendChild(navbarIcon);
+
+  // Agrego los elementos del navbar a la pagina
+  const page = document.getElementById("page");
+  nav.appendChild(logo);
+  nav.appendChild(navbarContainer);
+  nav.appendChild(navbarButton);
+  header.appendChild(nav);
+  page.prepend(header);
+});
+
+// Función para abrir el navbar
+window.openNavbar = () => {
+  const navbar = document.getElementById("navbar-container");
+  navbar.classList.toggle("active");
 };
--- client/script/productos.js
+++ client/script/productos.js
@@ -19,22 +19,74 @@ const mostrarProductos = async (containerProductId) => {
 
   //por cada element de la api creo un div
   productos.forEach((producto) => {
-    const element = document.createElement("li");
-    element.classList.add("product-item");
+    // Item
+    const li = document.createElement("li");
+    li.classList.add("product-item");
+    // Imagen
+    const figure = document.createElement("figure");
+    figure.classList.add("product-item-img");
+    const img = document.createElement("img");
+    img.src = producto.img;
+    img.alt = producto.name;
+    figure.appendChild(img);
 
+    // Informacio producto
+    const body = document.createElement("div");
+    body.classList.add("product-item-body");
+    const h3 = document.createElement("h3");
+    h3.textContent = producto.name;
+    const span = document.createElement("span");
+    span.textContent = "$200";
+    body.appendChild(h3);
+    body.appendChild(span);
+
+    // Botones
+    const botones = document.createElement("div");
+    botones.classList.add("product-item-btns");
+    const botonEditar = document.createElement("button");
+    // botonEditar.id = producto.name;
+    botonEditar.classList.add("btn", "btn-icon", "btn-blue");
+    botonEditar.setAttribute("onclick", `editarProducto(${producto.id})`);
+    botonEditar.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    botones.appendChild(botonEditar);
+
+    // Agrego los elementos
+    li.appendChild(figure);
+    li.appendChild(body);
+    li.appendChild(botones);
+    containerProduct.appendChild(li);
     //creo lo que se vera en el div
-    element.innerHTML = `
-            <figure class="product-item-img">
-              <img src="${producto.img}" alt="${producto.name}">
-            </figure>
-            <div class="product-item-body">
-              <h3>${producto.name}</h3>
-              <span>$200</span>
-            </div>
-        `;
+    // element.innerHTML = `
+    //         <figure class="product-item-img">
+    //           <img src="${producto.img}" alt="${producto.name}">
+    //         </figure>
+    //         <div class="product-item-body">
+    //           <h3>${producto.name}</h3>
+    //           <span>$200</span>
+    //         </div>
+    //         <div class="product-item-btns">
+    //           <button title="Editar" onClick={editarProducto(${producto})} class="btn btn-icon btn-blue"><i class="fa-solid fa-pen"></i></button>
+    //           <button title="Eliminar" class="btn btn-icon btn-red"><i class="fa-solid fa-trash"></i></button>
+    //         </div>
+    //     `;
 
-    containerProduct.appendChild(element);
+    // containerProduct.appendChild(element);
   });
+};
+
+const mostarModal = () => {
+  const modal = document.createElement("div")
+  
+}
+
+window.editarProducto = (producto) => {
+  // const modal = document.createElement("div")
+  // modal.classList.add("modal")
+  // const modalContainer = document.createElement("div")
+
+  // modalContainer
+  // console.log("click");
+  // console.log(producto);
 };
 
 mostrarProductos("shop-container");
