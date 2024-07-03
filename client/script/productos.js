b9e1fd3aaa821e6996b27a32d4fb980d95b55fc2
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-07-03 20:03:16 -0300

    add: funcion mostrarModal

--- client/script/productos.js
+++ client/script/productos.js
@@ -1,3 +1,4 @@
+// Funcion obtener todos los productos
 const obtenerProductos = async () => {
   const products = [];
   const data = await fetch("../data/data.json");
@@ -10,6 +11,12 @@ const obtenerProductos = async () => {
   return products;
 };
 
+// Funcion obtener producto por id
+const obtenerProductoPorId = async (id) => {
+  const productos = await obtenerProductos();
+  return productos.find((producto) => producto.id === id);
+};
+
 //funcion para mostrar los productos
 const mostrarProductos = async (containerProductId) => {
   //consumo la api
@@ -19,96 +26,93 @@ const mostrarProductos = async (containerProductId) => {
 
   //por cada element de la api creo un div
   productos.forEach((producto) => {
-    // Item
     const li = document.createElement("li");
     li.classList.add("product-item");
-    // Imagen
-    const figure = document.createElement("figure");
-    figure.classList.add("product-item-img");
-    const img = document.createElement("img");
-    img.src = producto.img;
-    img.alt = producto.name;
-    figure.appendChild(img);
+    li.innerHTML = `
+      <figure class="product-item-img">
+        <img src="${producto.img}" alt="${producto.name}"/>
+      </figure>
+      <div class="product-item-body">
+        <h3>${producto.name}</h3>
+        <span>$${producto.price}</span>
+      </div>
+      <div class="product-item-btns">
+        <button class="btn-icon btn-blue" title="Editar producto" onclick="editarProducto(${producto.id})">
+          <i class="fa-solid fa-pen"></i>
+        </button>
+        <button class="btn-icon btn-red" title="Eliminar producto" onclick="eliminarProducto(${producto.id})">
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    `;
 
-    // Informacio producto
-    const body = document.createElement("div");
-    body.classList.add("product-item-body");
-    const h3 = document.createElement("h3");
-    h3.textContent = producto.name;
-    const span = document.createElement("span");
-    span.textContent = "$200";
-    body.appendChild(h3);
-    body.appendChild(span);
-
-    // Botones
-    const botones = document.createElement("div");
-    botones.classList.add("product-item-btns");
-    const botonEditar = document.createElement("button");
-    // botonEditar.id = producto.name;
-    botonEditar.classList.add("btn-icon", "btn-blue");
-    botonEditar.setAttribute("onclick", `editarProducto(${producto.id})`);
-    botonEditar.innerHTML = '<i class="fa-solid fa-pen"></i>';
-    botones.appendChild(botonEditar);
-
-    // Agrego los elementos
-    li.appendChild(figure);
-    li.appendChild(body);
-    li.appendChild(botones);
-    containerProduct.appendChild(li);
-    //creo lo que se vera en el div
-    // element.innerHTML = `
-    //         <figure class="product-item-img">
-    //           <img src="${producto.img}" alt="${producto.name}">
-    //         </figure>
-    //         <div class="product-item-body">
-    //           <h3>${producto.name}</h3>
-    //           <span>$200</span>
-    //         </div>
-    //         <div class="product-item-btns">
-    //           <button title="Editar" onClick={editarProducto(${producto})} class="btn btn-icon btn-blue"><i class="fa-solid fa-pen"></i></button>
-    //           <button title="Eliminar" class="btn btn-icon btn-red"><i class="fa-solid fa-trash"></i></button>
-    //         </div>
-    //     `;
-
-    // containerProduct.appendChild(element);
+    containerProduct.prepend(li);
   });
 };
 
-const mostarModal = () => {
+const mostarModal = (producto) => {
   // Modal
   const modal = document.createElement("div");
   modal.classList.add("modal");
+
   // Modal container
   const modalContainer = document.createElement("div");
   modalContainer.classList.add("modal-container");
   modal.appendChild(modalContainer);
-  // Boton cerrar modal
+
+  // Botones
   const botonCerrarModal = document.createElement("button");
   botonCerrarModal.classList.add("closeModal");
   botonCerrarModal.innerHTML = '<i class="fa-solid fa-x"></i>';
   modalContainer.appendChild(botonCerrarModal);
+
+  // Eventos
   modal.addEventListener("click", () => {
-    page.removeChild(modal);
+    if (page.contains(modal)) {
+      page.removeChild(modal);
+    }
   });
   modalContainer.addEventListener("click", (e) => {
     e.stopPropagation();
   });
   botonCerrarModal.addEventListener("click", () => {
-    page.removeChild(modal);
+    if (page.contains(modal)) {
+      page.removeChild(modal);
+    }
   });
+
+  // Formulario editar producto
+  const formularioEditar = document.createElement("form");
+  formularioEditar.classList.add("form");
+  formularioEditar.innerHTML = `
+    <div class="form-field">
+      <label for="name">Nombre</label>
+      <input  type="text" name="name" id="name" value="${producto.name}"/>
+    </div>
+    <div class="form-field">
+      <label for="price">Precio</label>
+      <input type="text" name="price" id="price" value="${producto.price}" />
+    </div>
+  `;
+
+  modalContainer.appendChild(formularioEditar);
+
+  console.log(producto);
 
   const page = document.getElementById("page");
   page.appendChild(modal);
 };
 
-window.editarProducto = (producto) => {
-  mostarModal();
-  // const modal = document.createElement("div")
-  // modal.classList.add("modal")
-  // const modalContainer = document.createElement("div")
-  // modalContainer
-  // console.log("click");
-  // console.log(producto);
+// Funcion editar producto
+window.editarProducto = async (id) => {
+  const producto = await obtenerProductoPorId(id);
+  mostarModal(producto);
+};
+
+// Funcion eliminar producto
+window.eliminarProducto = async (id) => {
+  const producto = await obtenerProductoPorId(id);
+  console.log(`${producto.name} se elimino exitosamente!`);
 };
 
 mostrarProductos("shop-container");
