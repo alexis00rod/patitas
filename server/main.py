f893d9f592204563d895f2d1d4e065b7b5fda49f
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-07-02 21:15:54 -0300

    limpie codigo

--- server/main.py
+++ server/main.py
@@ -114,5 +114,5 @@ def update_producto(producto_id):
         return jsonify({"msg": "Error al actualizar el producto"}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 37450))
     app.run(host='0.0.0.0', port=port, debug=True)
